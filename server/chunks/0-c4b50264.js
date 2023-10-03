591842fe6c7e645edf2acc528b5d5919c660470e
foryouandyourcustomers <m@tias.me>
2023-10-03 10:13:50 +0000

    Update from https://github.com/foryouandyourcustomers/the-hidden-threat/commit/4dc52d2f432e460766f3fd416fdfe8a502c8a644

---
+++ server/chunks/0-c4b50264.js
@@ -0,0 +1,9 @@
+const index = 0;
+let component_cache;
+const component = async () => component_cache ??= (await import('./_layout.svelte-0c9ce180.js')).default;
+const imports = ["_app/immutable/nodes/0.c80d619b.js","_app/immutable/chunks/scheduler.60fb5a58.js","_app/immutable/chunks/index.02ea6f2d.js","_app/immutable/chunks/stores.907522b9.js","_app/immutable/chunks/singletons.aa078f3d.js","_app/immutable/chunks/index.c3580b25.js"];
+const stylesheets = ["_app/immutable/assets/0.a50ce878.css"];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets };
+//# sourceMappingURL=0-c4b50264.js.map
---
+++ server/chunks/utils-d2797627.js
@@ -0,0 +1,29 @@
+const getSharedGameContext = (context) => ({
+  timestamp: context.timestamp,
+  gameId: context.gameId,
+  hostUserId: context.hostUserId,
+  finishedAssigningSides: context.finishedAssigningSides,
+  globalAttackScenario: context.globalAttackScenario,
+  targetedAttacks: context.targetedAttacks,
+  users: context.users,
+  events: context.events,
+  defense: context.defense,
+  attack: context.attack
+});
+const createDefaultDefender = (hostUserId, id) => ({
+  id,
+  userId: hostUserId,
+  faceId: 0,
+  character: "dispatch-manager",
+  isConfigured: false
+});
+const createDefaultAttacker = (hostUserId) => ({
+  id: "attacker",
+  userId: hostUserId,
+  faceId: 0,
+  character: "disappointed",
+  isConfigured: false
+});
+
+export { createDefaultAttacker as a, createDefaultDefender as c, getSharedGameContext as g };
+//# sourceMappingURL=utils-d2797627.js.map
